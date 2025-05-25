37629f59e7286164dffba57658066eed3b99a774
Dan Brown <danbrown@backtablestudios.com>
2025-05-24 19:09:40 -0700

    v13 update

--- backgroundless-pins.js
+++ backgroundless-pins.js
@@ -1,46 +1,48 @@
 Hooks.on("init", () => {
     // Override map notes to use the BackgroundlessControlIcon
-    Note.prototype._drawControlIcon = function () {
-        const iconData = getIconData(this);
+    foundry.canvas.placeables.Note.prototype._drawControlIcon = function () {
+        const iconData = {
+            texture: this.document.texture.src,
+            size: this.document.iconSize,
+            tint: Color.from(this.document.texture.tint || null)
+        };
         const hasBackground = this.document.getFlag(
             "backgroundless-pins",
             "hasBackground"
         );
         const IconClass = hasBackground
-            ? ControlIcon
+            ? foundry.canvas.containers.ControlIcon
             : BackgroundlessControlIcon;
         const icon = new IconClass(iconData);
-        icon.x -= this.size / 2;
-        icon.y -= this.size / 2;
+        const halfIconSize = this.document.iconSize / 2;
+        icon.x -= halfIconSize;
+        icon.y -=halfIconSize;
         return icon;
     };
 });
 
-function getIconData(note) {
-    const tint = isV10OrLater()
-        ? Color.from(note.document.texture.tint || null)
-        : note.data.iconTint
-            ? colorStringToHex(note.data.iconTint)
-            : null;
-
-    const texture = isV10OrLater() ? note.document.texture.src : note.data.icon;
-
-    return { texture, size: note.size, tint };
-}
-
-Hooks.on("renderNoteConfig", (noteConfig, html, _) => {
+Hooks.on("renderNoteConfig", (noteConfig, html, data, options) => {
     const hasBackground = noteConfig.document.getFlag("backgroundless-pins", "hasBackground") ?? false;
-    const iconTintGroup = html.find("[name='texture.tint']").closest(".form-group");
-    iconTintGroup.after(`
-        <div class="form-group">
-            <label for="flags.backgroundless-pins.hasBackground">Show Background?</label>
-            <input type="checkbox" name="flags.backgroundless-pins.hasBackground" data-dtype="Boolean" ${hasBackground ? "checked" : ""}>
-        </div>
-    `);
+    const iconTintGroup = html.querySelector("[name='texture.tint']").closest(".form-group");
+
+    const bgOption = document.createElement("div");
+    bgOption.classList.add("form-group");
+    bgOption.innerHTML = `
+        <label for="flags.backgroundless-pins.hasBackground">Show Background?</label>
+        <div class="form-fields"><input type="checkbox" name="flags.backgroundless-pins.hasBackground" data-dtype="Boolean" ${hasBackground ? "checked" : ""}></div>
+        `;
+
+    iconTintGroup.parentElement.appendChild(bgOption);
+
     noteConfig.setPosition({ height: "auto" });
 });
 
-export class BackgroundlessControlIcon extends ControlIcon {
+Hooks.on("closeNoteConfig", (noteConfig) => {
+    //There seems to be no way to trigger the draw as part of changing the hasBackground value so we just do it here every time
+    noteConfig.document.object.draw({force: true});
+});
+
+export class BackgroundlessControlIcon extends foundry.canvas.containers.ControlIcon {
     /**
      * Override ControlIcon#draw to remove drawing of the background.
      */
@@ -49,7 +51,7 @@ export class BackgroundlessControlIcon extends ControlIcon {
         if (this.destroyed) return this;
 
         // Load the icon texture
-        this.texture = this.texture ?? await loadTexture(this.iconSrc);
+        this.texture = this.texture ?? await foundry.canvas.loadTexture(this.iconSrc);
 
         // Set the icon texture
         this.icon.texture = this.texture;
@@ -63,8 +65,4 @@ export class BackgroundlessControlIcon extends ControlIcon {
         // Refresh
         return this.refresh();
     }
-}
-
-function isV10OrLater() {
-    return game.release.generation >= 10;
 }
